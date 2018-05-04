336eba8591e8d8891a5c37af9a0e7b16a0b718fe
Mike Huang <mikemike07@users.noreply.github.com>
2018-05-03 22:54:32 -0400

    sprint3

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -1,7 +1,7 @@
 var modal = document.getElementById('mypop');
 
 // Get the button that opens the modal
-var btn = document.getElementById("lookup");
+var btn = document.getElementById("lookupmain");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
@@ -22,3 +22,24 @@ window.onclick = function(event) {
         modal.style.display = "none";
     }
 }
+
+
+
+
+
+var bar = new ProgressBar.Circle(container, {
+  color: '#FFEA82',
+  trailColor: '#eee',
+  trailWidth: 1,
+  duration: 1400,
+  easing: 'bounce',
+  strokeWidth: 6,
+  from: {color: '#FFEA82', a:0},
+  to: {color: '#ED6A5A', a:1},
+  // Set default step function for all animate calls
+  step: function(state, circle) {
+    circle.path.setAttribute('stroke', state.color);
+  }
+});
+
+bar.animate(1.0); 
